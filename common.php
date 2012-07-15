d467d14cb4618355d5053786a402b6832143b5a6
trox <trox@trox-PC>
2012-07-15 12:51:31 +0200

    Some cleanup changes Removal of Zend Framework Custom Autoloader

--- common.php
+++ common.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 
 date_default_timezone_set('Europe/Zurich');
@@ -8,11 +9,8 @@ define('__LIB__', 'lib');
 
 set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ . '/lib');
 
-require_once 'lib/Zend/Loader/StandardAutoloader.php';
-$autoLoader = new Zend\Loader\StandardAutoloader(array(
-            'fallback_autoloader' => true,
-            'namespaces' => array(
-                'yasCMS' => __LIB__ . '/yasCMS'
-            )
-        ));
-$autoLoader->register();
+require_once 'lib/yasCMS/Loader/Autoloader.php';
+
+$yasLoader = new yasCMS\Loader\AutoLoader();
+$yasLoader->registerNamespace('yasCMS', __LIB__ );
+$yasLoader->register();
---
+++ lib/yasCMS/Loader/ModuleLoader.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace yasCMS\Loader;
+
+class ModuleLoader
+{
+
+    private static $loadedModules = array();
+
+    public static function loadModule($moduleName)
+    {
+        if (!in_array($moduleName, self::$loadedModules)) {
+            include_once 'modules/' . $moduleName . '.php';
+            self::$loadedModules[] = $moduleName;
+        }
+    }
+
+}
